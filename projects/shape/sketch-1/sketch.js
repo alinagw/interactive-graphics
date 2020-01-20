1af860dbb687cabd787762bfac24bd0319614b96
Alina Walling <alina.walling@gmail.com>
2020-01-19 19:05:05 -0500

    experimenting with sketch 1 painting in 3D

--- projects/shape/sketch-1/sketch.js
+++ projects/shape/sketch-1/sketch.js
@@ -1,7 +1,30 @@
+var w = 600;
+var h = 600;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(w, h, WEBGL);
+    background(200);
   }
   
   function draw() {
-    background(220);
+    if (keyIsDown(32)) {
+      orbitControl(5,5,5);
+    }
+  }
+
+  function mousePressed() {
+   //paint();
+  }
+
+  function mouseDragged() {
+    if (!keyIsDown(32)) {
+      paint();
+    }
+  }
+
+  function paint() {
+    translate(mouseX - w / 2, mouseY - h / 2);
+    //noStroke();
+    fill(255,0,0);
+    sphere(40);
   }
