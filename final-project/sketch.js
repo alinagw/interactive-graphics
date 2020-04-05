cf256d2ec05c3ef40113202d67eaa2fd024a4dce
Alina Walling <alina.walling@gmail.com>
2020-04-05 17:37:24 -0400

    set up video input, posenet, and drawing skeleton

--- final-project/sketch.js
+++ final-project/sketch.js
@@ -1,7 +1,46 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-    background(220);
+    image(video, 0, 0);
+    if (pose && skeleton) {
+        drawSkeleton();
+    }
+}
+
+function modelLoaded() {
+    console.log("PoseNet ready!");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        pose = results[0].pose;
+        skeleton = results[0].skeleton;
+    }
+}
+
+function drawSkeleton() {
+    for (let i = 0; i < pose.keypoints.length; i++) {
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        ellipse(x, y, 15, 15);
+    }
+
+    for (let i = 0; i < skeleton.length; i++) {
+        let a = skeleton[i][0];
+        let b = skeleton[i][1];
+        strokeWeight(2);
+        stroke(255);
+        line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
 }
